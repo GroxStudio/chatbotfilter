a6a48ed4fea33e94dd5c0acf847b01162c696ef9
Дмитрий Малеев <65185463+GroxStudio@users.noreply.github.com>
2020-05-15 14:51:55 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,20 +6,22 @@ from discord.utils import get
 
 bot = commands.Bot(command_prefix='f.') #инициализируем бота с префиксом 'f.'
 
-bad_words = [ 'сука', 'хуй', 'пздц', 'пиздец', 'хуёк' ]
+bad_words = 'сука', 'хуй', 'пздц', 'пиздец', 'хуёк'
 
 
 
 
 @bot.event
 async def on_message(message):
-	await bot.procces_commands(message)
-
+	await bot.process_commands(message)
 	msg = message.content.lower()
-
+	
+	
 	if msg in bad_words:
 		await message.delete()
-		await message.author.send(f {message.author.name}, Не матерись на сервере!)
+		await message.author.send(embed = discord.Embed(description = f'{message.author}, не матерись!!!',color=0x0c0c0c)) 
+
+        return
 
 
 
